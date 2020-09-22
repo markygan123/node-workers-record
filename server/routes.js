1e608da7db18e932faf4de7fd2f80d6b9e5359c8
markygan123 <macawilemarkygan@gmail.com>
2020-09-22 01:26:18 -0500

    add edit record and delete record functionalities

--- server/routes.js
+++ server/routes.js
@@ -37,9 +37,74 @@ router.post('/newRecord', (req, res) => {
                         return res.redirect('/');
                     }
                 });
-    // DB.query(console.log(record));
 });
 
+
+router.get('/editRecord/:id', (req, res) => {
+    DB.query(`SELECT * from workers WHERE id = ${req.params.id} LIMIT 1`, (error, result) => {
+        if (error) {
+            console.log('Error: ');
+            console.log(error);
+            return res.redirect('/');
+        } else {
+            return res.render('editRecord', {
+                id: req.params.id,
+                name: result[0].name,
+                trade: result[0].trade,
+                phone_no: result[0].phone_no                
+            });
+        }
+    });
+});
+
+
+router.post('/editRecord/:id', (req, res) => {
+    const record = req.body;
+    DB.query(`UPDATE workers SET name = '${record.name}',
+                trade = '${record.trade}',
+                phone_no = '${record.phone_no}'
+                WHERE id = ${req.params.id}`, (error, result) => {
+                    if (error) {
+                        console.log('Error: ');
+                        console.log(error);
+                        return res.redirect('/editRecord/:id')
+                    } else {
+                        return res.redirect('/')
+                    }
+                });
+});
+
+
+router.get('/deleteRecord/:id', (req, res) => {
+    DB.query(`SELECT * from workers WHERE id = ${req.params.id} LIMIT 1`, (error, result) => {
+        if (error) {
+            console.log('Error: ');
+            console.log(error);
+            return res.redirect('/');
+        } else {
+            return res.render('deleteRecord', {
+                id: req.params.id,
+                name: result[0].name,
+                trade: result[0].trade,
+                phone_no: result[0].phone_no                
+            });
+        }
+    });
+});
+
+
+router.get('/deleteRecord/:id/delete', (req, res) => {
+    const post = req.body;
+    DB.query(`DELETE FROM workers WHERE id = ${req.params.id}`, (error, result) => {
+        if (error) {
+            console.log('Error: ');
+            console.log(error);
+            return res.redirect(`/deleteRecord/${req.params.id}/delete`);
+        } else {
+            return res.redirect(`/`);
+        }
+    });
+});
 
 
 module.exports = router;
